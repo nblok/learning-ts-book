d60600b18f879bb5888ae3c7fb44990916d2ae03
Nathan Blok <ndblok@gmail.com>
2015-10-05 06:58:50 -0700

    Updated gulp file to compile typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,35 @@
 /* Gulp file */
 var gulp = require('gulp'),
-    tslint = require('gulp-tslint');
+    tslint = require('gulp-tslint'),
+    ts = require('gulp-typescript');
+
+var tsProject = ts.createProject({
+        removeComments: true,
+        noImplicitAny: true,
+        target: 'ES3',
+        module: 'commonjs',
+        declarationFiles: false
+    }),
+    tsSource = './src/ts/**/**.ts',
+    tsTest = './test/**/*.test.ts';
 
 gulp.task('lint', function() {
     return gulp.src([
-        './src/ts/**/**.ts', './test/**/*.test.ts'
+        tsSource, tsTest
     ]).pipe(tslint())
       .pipe(tslint.report('verbose'));
 });
 
-gulp.task('default', ['lint']);
+gulp.task('tsc', function() {
+    return gulp.src(tsSource)
+               .pipe(ts(tsProject))
+               .js.pipe(gulp.dest('./build/src/js'));
+});
+
+gulp.task('tsc-test', function() {
+    return gulp.src(tsTest)
+               .pipe(ts(tsProject))
+               .js.pipe(gulp.dest('./build/test/js'));
+});
+
+gulp.task('default', ['lint', 'tsc', 'tsc-test']);
