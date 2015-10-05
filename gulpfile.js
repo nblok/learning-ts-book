126d29f1e316075fb5a324190fb5c902d7a93c97
Nathan Blok <ndblok@gmail.com>
2015-10-05 06:46:06 -0700

    Added TypeScript linting to Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
 /* Gulp file */
+var gulp = require('gulp'),
+    tslint = require('gulp-tslint');
+
+gulp.task('lint', function() {
+    return gulp.src([
+        './src/ts/**/**.ts', './test/**/*.test.ts'
+    ]).pipe(tslint())
+      .pipe(tslint.report('verbose'));
+});
+
+gulp.task('default', ['lint']);
